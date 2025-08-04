8cfb73a29837eb1b5d9ed8f0e6b26b5597bb96db
Cart <81428538+cartrigger@users.noreply.github.com>
2025-08-04 04:00:51 -0400

    Fix Slimeballs

--- src/main/java/com/simibubi/create/foundation/data/recipe/CreateStandardRecipeGen.java
+++ src/main/java/com/simibubi/create/foundation/data/recipe/CreateStandardRecipeGen.java
@@ -25,6 +25,8 @@ import io.github.fabricators_of_create.porting_lib.resources.conditions.IConditi
 import io.github.fabricators_of_create.porting_lib.resources.conditions.ModLoadedCondition;
 import io.github.fabricators_of_create.porting_lib.resources.conditions.NotCondition;
 
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
+
 import org.jetbrains.annotations.Nullable;
 
 import com.google.common.base.Supplier;
@@ -313,7 +315,7 @@ public final class CreateStandardRecipeGen extends BaseRecipeProvider {
 				.pattern(" B ")),
 
 		SUPER_GLUE = create(AllItems.SUPER_GLUE).unlockedByTag(I::ironSheet)
-			.viaShaped(b -> b.define('A', Items.SLIME_BALL)
+			.viaShaped(b -> b.define('A', ConventionalItemTags.SLIME_BALLS)
 				.define('S', I.ironSheet())
 				.define('N', Tags.Items.NUGGETS_IRON)
 				.pattern("AS")
@@ -382,7 +384,7 @@ public final class CreateStandardRecipeGen extends BaseRecipeProvider {
 				.pattern("I")),
 
 		STICKY_MECHANICAL_PISTON = create(AllBlocks.STICKY_MECHANICAL_PISTON).unlockedBy(I::andesiteAlloy)
-			.viaShaped(b -> b.define('S', Tags.Items.SLIMEBALLS)
+			.viaShaped(b -> b.define('S', ConventionalItemTags.SLIME_BALLS)
 				.define('P', AllBlocks.MECHANICAL_PISTON.get())
 				.pattern("S")
 				.pattern("P")),
@@ -859,7 +861,7 @@ public final class CreateStandardRecipeGen extends BaseRecipeProvider {
 			.viaShaped(b -> b.define('I', I.andesiteAlloy())
 				.define('C', Tags.Items.COBBLESTONES)
 				.define('R', I.redstone())
-				.define('S', Tags.Items.SLIMEBALLS)
+				.define('S', ConventionalItemTags.SLIME_BALLS)
 				.pattern("ISI")
 				.pattern("CRC")),
 
@@ -1113,7 +1115,7 @@ public final class CreateStandardRecipeGen extends BaseRecipeProvider {
 
 		PACKAGE_FROGPORT = create(AllBlocks.PACKAGE_FROGPORT).unlockedBy(I::cardboard)
 			.viaShaped(b -> b.define('C', I.andesiteAlloy())
-				.define('B', Tags.Items.SLIMEBALLS)
+				.define('B', ConventionalItemTags.SLIME_BALLS)
 				.define('A', I.vault())
 				.pattern("B")
 				.pattern("A")
