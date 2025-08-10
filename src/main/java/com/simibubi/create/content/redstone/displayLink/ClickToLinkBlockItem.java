d87e7e21425bcff6f10dbca1246bf22d4a6f9c82
Naz <bluspringindohq@gmail.com>
2025-08-10 18:29:13 +0800

    actually, flip that

--- src/main/java/com/simibubi/create/content/redstone/displayLink/ClickToLinkBlockItem.java
+++ src/main/java/com/simibubi/create/content/redstone/displayLink/ClickToLinkBlockItem.java
@@ -61,7 +61,7 @@ public abstract class ClickToLinkBlockItem extends BlockItem {
 			.getBlockState(event.getPos())
 			.is(blockItem.getBlock()))
 			return;
-		event.setCancellationResult(InteractionResult.SUCCESS);
+		event.setCancellationResult(InteractionResult.FAIL);
 	}
 
 	@Override
